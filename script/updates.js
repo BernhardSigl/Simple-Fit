da6e6c6f2e0bbeafdf9131b845b2d6e2e7172d56
Bernhard Sigl <b-sigl@live.de>
2023-11-30 01:32:45 +0100

    add updates section

---
+++ script/updates.js
@@ -0,0 +1,47 @@
+function showUpdates() {
+    changeShowUpdatesFunction();
+    document.getElementById('settingsImgId').src = 'img/backArrow.png';
+    toggleVisibilityById('logOutBtnId', false);
+    toggleVisibilityById('settingsImgId', true);
+    toggleVisibilityById('welcomeMessageId', false);
+    toggleVisibilityById('btnAreaId', false);
+    toggleVisibilityById('gymDiaryBtns', false);
+    toggleVisibilityById('scrollboxId', true);
+    toggleVisibilityById('dumbellId', false);
+    toggleVisibilityById('gymDiaryTitleId', false);
+    toggleVisibilityById('gymDiaryAreaId', false);
+    toggleVisibilityById('updatesTitleId', true);
+    toggleVisibilityById('updatesDescriptionId', true);
+    toggleVisibilityById('updateImgId', false);
+}
+
+function fromUpdatesToMenu() {
+    originalShowSettingsFunction();
+    document.getElementById('settingsImgId').src = 'img/settings.png';
+    toggleVisibilityById('logOutBtnId', true);
+    toggleVisibilityById('settingsImgId', true);
+    toggleVisibilityById('welcomeMessageId', true);
+    toggleVisibilityById('btnAreaId', true);
+    toggleVisibilityById('gymDiaryBtns', true);
+    toggleVisibilityById('scrollboxId', true);
+    toggleVisibilityById('dumbellId', true);
+    toggleVisibilityById('gymDiaryTitleId', true);
+    toggleVisibilityById('gymDiaryAreaId', true);
+    toggleVisibilityById('updatesTitleId', false);
+    toggleVisibilityById('updatesDescriptionId', false);
+    toggleVisibilityById('updateImgId', true);
+}
+
+function changeShowUpdatesFunction() {
+    const changeShowUpdatesBtn = document.getElementById('settingsImgId');
+    changeShowUpdatesBtn.onclick = function () {
+        fromUpdatesToMenu();
+    };
+}
+
+// function originalChangeUpdateFunction() {
+//     const changeShowUpdatesBtn = document.getElementById('editContactFormId');
+//     changeShowUpdatesBtn.onclick = function () {
+//         createContact();
+//     };
+// }
